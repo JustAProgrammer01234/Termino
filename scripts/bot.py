0b22f7df943386d7bf48635d545851c1e0661d99
JustAProgrammer01234 <justaprogrammerlol@gmail.com>
2021-04-18 11:03:27 +0800

    Removed bubble_sort and insertion_sort command because it causes too much dirtiness and modified bot.py and noncoroutines/funcs.py.

--- scripts/bot.py
+++ scripts/bot.py
@@ -1,4 +1,3 @@
-import time
 import random
 import discord
 from converters import *
@@ -11,49 +10,14 @@ termino = commands.Bot(command_prefix = '$', help_command = None, intents = inte
 @termino.event
 async def on_ready():
     print(f'{termino.user.name} is ready to go.')
-    print('-----------------------------------')
-
-@termino.command()
-async def bubble_sort(ctx, array, mode):
-    array = str_to_array(array)
-    if len(array) <= 10:
-            if mode == 'ascending':
-                elapsed = run_bubble_sort_ascending(array)
-                await ctx.send(f'Finished sorting array: {array} in {elapsed} seconds.')
-
-            elif mode == 'descending':
-                elapsed = run_bubble_sort_descending(array)
-                await ctx.send(f'Finished sorting array: {array} in {elapsed} seconds.')
-
-            else:
-                await ctx.send(f'Error:\nWrong value of mode: {mode}\nMust be ascending or descending.')
-    else:
-        await ctx.send('Length of array must not be greater than ten.')
-
-
-@termino.command()
-async def insertion_sort(ctx, array, mode):
-    array = str_to_array(array)
-    if len(array) <= 10:
-        if mode == 'ascending':
-            elapsed = run_insertion_sort_ascending(array)
-            await ctx.send(f'Finished sorting array: {array} in {elapsed} seconds.')
-
-        elif mode == 'descending':
-            elapsed = run_insertion_sort_descending(array)
-            await ctx.send(f'Finished sorting array: {array} in {elapsed} seconds.')
-
-        else:
-            await ctx.send(f'Error: wrong value of mode: {mode}\nMust be ascending or descending.')
-    else:
-        await ctx.send('Length of array must not be greater than ten.')
+    print('-----------------------------------'))
 
 @termino.command()
 async def help(ctx):
     await ctx.send(get_help())
 
 @termino.command()
-async def display(ctx, message):
+async def display(ctx,*, message):
     await ctx.send(message)
 
 @termino.command()
