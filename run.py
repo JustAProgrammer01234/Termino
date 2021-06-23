4bbd5950215d976764ef1fecdd39c080fe570e4d
JustAProgrammer01234 <JustAProgrammerlol@gmail.com>
2021-06-23 18:16:25 +0800

    changes changes

--- run.py
+++ run.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     termino.load_extension('bot')
     termino.load_extension('helpcommand')
     for content in os.listdir():
-        if os.path.isdir(content) and content not in ['noncoroutines','cmdlist', '__pycache__']:
+        if os.path.isdir(content) and content not in ['noncoroutines','cmdlist', '__pycache__'] and not content.startswith('.'):
             termino.load_extension(content)
     termino.run(get_token('token.txt'))
