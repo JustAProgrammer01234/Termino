c944ffb3d01e07c92316e7fb6239b179302bc536
JustAProgrammer01234 <JustAProgrammerlol@gmail.com>
2021-07-05 13:49:16 +0800

    made some slight changes in the help command.

--- helpcommand.py
+++ helpcommand.py
@@ -20,18 +20,18 @@ class Help(commands.Cog):
 
     @help.command()
     async def fun(self, ctx):
-        embd = discord.Embed(title = 'These are the commands for fun!', description = get_help_fun(), color = discord.Colour.green())
+        embd = discord.Embed(title = ':basketball: For fun :basketball:', description = get_help_fun(), color = discord.Colour.green())
         await ctx.send(embed = embd)
 
     @help.command()
     async def mod(self, ctx):
-        embd = discord.Embed(title = 'Moderator commands.', description = get_help_mod(), color = discord.Colour.green())
+        embd = discord.Embed(title = ':shield: For moderation :shield:', description = get_help_mod(), color = discord.Colour.green())
         embd.set_footer(text = "Don't forget to sync my perms for them to work!")
         await ctx.send(embed = embd)
 
     @help.command()
     async def settings(self, ctx):
-        embd = discord.Embed(title = 'Termino settings.', color = discord.Colour.green())
+        embd = discord.Embed(title = ':gear: For settings :gear:', color = discord.Colour.green())
         embd.add_field(name = 'Welcomes:', value = get_help_util_welcome(), inline = False)
         embd.add_field(name = 'Mute command setup:', value = get_help_util_mute(), inline = False)
         embd.set_footer(text = "Don't forget to sync my perms for them to work!")
@@ -39,7 +39,7 @@ class Help(commands.Cog):
 
     @help.command()
     async def games(self, ctx):
-        embd = discord.Embed(title = 'Termino games.', description = get_help_game(), color = discord.Colour.green())
+        embd = discord.Embed(title = ':video_game: Games :video_game:', description = get_help_game(), color = discord.Colour.green())
         await ctx.send(embed = embd)
 
     @help.command()
