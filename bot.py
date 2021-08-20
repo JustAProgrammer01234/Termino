b0f23341ea1c9924c4c3212d4b3cbce15c084ea9
JustAProgrammer01234 <JustAProgrammerlol@gmail.com>
2021-08-20 17:36:31 +0800

    Hope this works.

--- bot.py
+++ bot.py
@@ -11,15 +11,15 @@ async def get_prefix(bot, message):
     return await bot.termino_servers.fetch_server_info(message.guild.id)['prefix']
 
 class Bot(commands.AutoShardedBot):
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         super().__init__(
-            command_prefix = get_prefix,
             intents = discord.Intents.all(),
             activity = discord.Game(name = 'for $.help'),
             help_command = TerminoHelp(),
             description = 'Just your average bot.',
             owner_id = 790767157523775518
         )
+        self.command_prefix = get_prefix
         self.reddit = reddit.SubReddit(client_id = os.getenv('REDDIT_CLIENT_ID'), 
                             client_secret = os.getenv('REDDIT_CLIENT_SECRET'), 
                             user_agent = os.getenv('REDDIT_USER_AGENT')
